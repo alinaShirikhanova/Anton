8e6d3277a71acb712b4fe28e58d5c27ab36ea0c2
alina <alina.meller.98@mail.ru>
2025-05-23 12:19:42 +0300

    08-05-2025

---
+++ src/map/hashmap/_08_05_2025/Main.java
@@ -0,0 +1,10 @@
+package map.hashmap._08_05_2025;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/map/hashmap/l_01_05_2025/MapDemo.java
+++ src/map/hashmap/l_01_05_2025/MapDemo.java
@@ -11,44 +11,47 @@ public class MapDemo {
         map.put("one", 1);
         map.put("two", 1);
         map.put("three", 1);
-        map.put(null, 0);
-        map.put("four", null);
+        map.replace("one", 1, 10);
+        System.out.println(map);
+//        map.put(null, 0);
+//        map.put("four", null);
+
 //        map.put("one", 1);
-        System.out.println(map.get("one"));
-//        if (map.containsKey("four"))
-//            System.out.println(map.get("four"));
-
-        Set<String> strings = map.keySet();
-        System.out.println(strings);
-        for (String key : strings) {
-            System.out.println(key + " : " + map.get(key));
-        }
-
-        Collection<Integer> values = map.values();
-        System.out.println(values);
-
-
-        Set<Map.Entry<String, Integer>> entries = map.entrySet();
-        for (Map.Entry<String, Integer> entry : entries){
-            System.out.println(entry.getValue());
-        }
-
-        Map<Integer, Car> cars = new HashMap<>();
-        cars.put(1, new Car("Лада"));
-        cars.put(2, new Car("Лада"));
-        cars.put(3, new Car("Лада"));
-        cars.put(4, new Car("Лада"));
-
-        for (Map.Entry<Integer, Car> entry : cars.entrySet()) {
-            System.out.println(entry.getValue().getModel());
-        }
-
-        for (Map.Entry<Integer, Car> entry : cars.entrySet()) {
-            if (entry.getKey() == 1){
-                entry.setValue(new Car("БМВ"));
-            }
-        }
-        System.out.println(cars);
+//        System.out.println(map.get("one"));
+////        if (map.containsKey("four"))
+////            System.out.println(map.get("four"));
+//
+//        Set<String> strings = map.keySet();
+//        System.out.println(strings);
+//        for (String key : strings) {
+//            System.out.println(key + " : " + map.get(key));
+//        }
+//
+//        Collection<Integer> values = map.values();
+//        System.out.println(values);
+//
+//
+//        Set<Map.Entry<String, Integer>> entries = map.entrySet();
+//        for (Map.Entry<String, Integer> entry : entries){
+//            System.out.println(entry.getValue());
+//        }
+//
+//        Map<Integer, Car> cars = new HashMap<>();
+//        cars.put(1, new Car("Лада"));
+//        cars.put(2, new Car("Лада"));
+//        cars.put(3, new Car("Лада"));
+//        cars.put(4, new Car("Лада"));
+//
+//        for (Map.Entry<Integer, Car> entry : cars.entrySet()) {
+//            System.out.println(entry.getValue().getModel());
+//        }
+//
+//        for (Map.Entry<Integer, Car> entry : cars.entrySet()) {
+//            if (entry.getKey() == 1){
+//                entry.setValue(new Car("БМВ"));
+//            }
+//        }
+//        System.out.println(cars);
 //        Распечатать ключи и значения тех людей, у которых возраст больше 10
     }
 }
